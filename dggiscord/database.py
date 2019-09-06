94faa91090ccef3fab6125f5f4fe5f3583f8e3bb
ILiedAboutCake <iliedaboutthecake@gmail.com>
2019-09-05 21:17:50 -0400

    Add automated schema creation, error handling in starting sqlite

--- dggiscord/database.py
+++ dggiscord/database.py
@@ -3,5 +3,19 @@ from log import logging
 import sqlite3
 import datetime
 
-con = sqlite3.connect(cfg['db'])
-cur = con.cursor()
+logger = logging.getLogger(__name__)
+logger.info("loading...")
+
+try:
+    con = sqlite3.connect(cfg['db'])
+    cur = con.cursor()
+    logger.info("sqlite database connection and cursor successfully initialized")
+except Exception as e:
+    logger.critical("sqlite database failed with error: {0}".format(e))
+    exit()
+
+# Check if the table exists, if not make the schema
+cur.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='flairmap'")
+if cur.fetchone()[0] == 0:
+    logger.warn("table 'flairmap' does not exist in database, creating schema...")
+    cur.execute("CREATE TABLE 'flairmap' ( `discord_server` INTEGER, `discord_role` INTEGER, `dgg_flair` TEXT, `last_updated` TEXT, `last_refresh` TEXT, PRIMARY KEY(`discord_role`) )")
